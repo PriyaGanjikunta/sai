191bb53c87782acbbb477b0cb2aa64c4fe55e505
PriyaGanjikunta <145114565+PriyaGanjikunta@users.noreply.github.com>
2024-12-30 23:48:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let ip = prompt("Enter your girl friend name");
+while(ip != "priya"){
+    ip = prompt("Enter correct name, your gf name didn't matched (correct ga enter chey ra kukkaaaa ledante miss aypotav ra reyyyyyyyyyyyyyyyy)try again!!");
+}
