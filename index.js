8227e10a1abf8b51f11b7376e8a28dba64ae2948
PriyaGanjikunta <145114565+PriyaGanjikunta@users.noreply.github.com>
2024-12-30 23:53:36 +0530

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-let ip = prompt("Enter your girl friend name");
-while(ip != "priya"){
-    ip = prompt("Enter correct name, your gf name didn't matched (correct ga enter chey ra kukkaaaa ledante miss aypotav ra reyyyyyyyyyyyyyyyy)try again!!");
-}
